d484cc1f3cb0d03f87a21cf98a1688212503ce54
arifmia1129 <arifmia1129@gmail.com>
2023-08-20 07:21:13 +0600

    user model added

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,54 @@
+import { Schema, model } from "mongoose";
+import { IUser, IUserMethods, UserModel } from "./user.interface";
+import bcrypt from "bcryptjs";
+import config from "../../../config";
+
+const userSchema = new Schema<IUser, UserModel, IUserMethods>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// use instance method to check user exist or not
+userSchema.methods.isUserExist = async function (
+  email: string,
+): Promise<IUser | null> {
+  return await User.findOne({ email });
+};
+
+// use instance method to check user login password is right or not
+userSchema.methods.isPasswordMatched = async function (
+  givenPassword: string,
+  savedPassword: string,
+): Promise<boolean> {
+  return await bcrypt.compare(givenPassword, savedPassword);
+};
+
+userSchema.pre("save", async function (next) {
+  // hashing password
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+const User = model<IUser, UserModel>("User", userSchema);
+
+export default User;
